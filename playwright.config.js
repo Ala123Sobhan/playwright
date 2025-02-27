c15b3ee0a4b65155ac324b3523a9c77c37070d09
Ala123Sobhan <ala123sobhan@gmail.com>
2025-02-27 14:35:11 -0600

    add config changes

--- playwright.config.js
+++ playwright.config.js
@@ -13,6 +13,12 @@ import { defineConfig, devices } from '@playwright/test';
  * @see https://playwright.dev/docs/test-configuration
  */
 export default defineConfig({
+
+ 
+  timeout: 60_000,
+  expect: {
+    timeout: 10_000
+  },
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
@@ -21,7 +27,7 @@ export default defineConfig({
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 3,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -30,7 +36,12 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on",
+    screenshot: "on",
+    video: "on",
+    headless: true,
+    baseURL: "https://opensource-demo.orangehrmlive.com",
+
   },
 
   /* Configure projects for major browsers */
@@ -65,10 +76,10 @@ export default defineConfig({
     //   name: 'Microsoft Edge',
     //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
     // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
+    {
+      name: 'Google Chrome',
+      use: { ...devices['Desktop Chrome'], channel: 'chrome' },
+    },
   ],
 
   /* Run your local dev server before starting the tests */
--- tests/orangehrmLogin.spec.js
+++ tests/orangehrmLogin.spec.js
@@ -1,4 +1,6 @@
+// @ts-check
 import { test, expect } from '@playwright/test';
+
 
 const testData = [
 
@@ -33,12 +35,12 @@ testData.forEach((data, index) =>{
 
     test(`Verify login feature for organgeHrm for ${index+1}`, async({page}) => {
 
-        await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+        await page.goto("/web/index.php/auth/login");
         await page.locator("//input[@placeholder='Username']").fill(data.username);
         await page.locator("input[type='password']").fill(data.password);
         await page.locator("button[type='submit']").click();
         data.status === "valid" ? 
-        await expect(page).toHaveURL("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index")
+        await expect(page).toHaveURL("/web/index.php/dashboard/index")
         : await expect(page.locator("//p[text()='Invalid credentials']")).toBeVisible();
 
 
