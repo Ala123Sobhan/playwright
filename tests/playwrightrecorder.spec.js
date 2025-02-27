a04df875044c7a03de238b2762825bb7a4ff7af4
Ala123Sobhan <ala123sobhan@gmail.com>
2025-02-27 15:36:25 -0600

    add code gen

---
+++ tests/playwrightrecorder.spec.js
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://demo.playwright.dev/todomvc/#/');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).click();
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('q');
+  await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+  await page.getByRole('link', { name: 'Active' }).click();
+  await page.getByRole('link', { name: 'Completed' }).click();
+  await page.getByText('All Active Completed').click();
+  await page.getByText('item leftAll Active Completed').click();
+});
